81ca4efab9fd40c9d10e556ce75d2489d293dfd9
jamalgithub <jelmoubarik@gmail.com>
2019-12-30 12:57:48 +0100

    Integreation de firebase à l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+
+  constructor() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCbcqLdhJOzBQZagQdnM7Ju-h0nbR88YPc",
+      authDomain: "bookselves-28db2.firebaseapp.com",
+      databaseURL: "https://bookselves-28db2.firebaseio.com",
+      projectId: "bookselves-28db2",
+      storageBucket: "bookselves-28db2.appspot.com",
+      messagingSenderId: "394844120940",
+      appId: "1:394844120940:web:676a5c432b2e69b157f549"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
